428b209e2852c02c897b3ab6922089aefa48b63a
xiangmingzhuo <97508342+xiangmingzhuo@users.noreply.github.com>
2025-10-31 13:27:52 +0800

    Delete code directory

--- code/approximateNumberDown.m
+++
@@ -1,4 +0,0 @@
-% m向下取整到n的最大倍数
-function approx = approximateNumberDown(num, interval)
-    approx = floor(num / interval) * interval;
-end
--- code/approximateNumberUp.m
+++
@@ -1,4 +0,0 @@
-% m向上取整到n的最大倍数
-function approx = approximateNumberUp(num, interval)
-    approx = ceil(num / interval) * interval;
-end
