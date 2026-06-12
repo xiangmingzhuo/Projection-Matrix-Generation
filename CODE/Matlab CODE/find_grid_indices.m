a068e8bed6a0a6bc9fdb8b323c8f4d783c478f7c
xiangmingzhuo <97508342+xiangmingzhuo@users.noreply.github.com>
2026-06-12 18:24:37 +0800

    Delete CODE/Matlab CODE directory

--- CODE/Matlab CODE/find_grid_indices.m
+++
@@ -1,7 +0,0 @@
-function grid_indices = find_grid_indices(midpoint_blh, jdmin, wdmin, gdmin, jdjg, wdjg, gdjg, jdmax, wdmax, gdmax, epsilon,Nx,Ny,Nz)
-    indices_x = ceil((midpoint_blh(:, 1) + epsilon - jdmin) / jdjg);
-    indices_y = ceil((midpoint_blh(:, 2) + epsilon - wdmin) / wdjg);
-    indices_z = ceil((midpoint_blh(:, 3) + epsilon - gdmin) / gdjg);
-
-    grid_indices = (indices_z - 1) * (Nx * Ny) + (indices_y - 1) * Nx + indices_x;
-end
